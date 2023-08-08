8e51de5c4bfeace12917219ad18cbbbc9087349a
Yim Chan <50952437+YeCheng57@users.noreply.github.com>
2023-08-08 15:09:35 +0800

    Update burden.R

--- Burden/burden.R
+++ Burden/burden.R
@@ -1,42 +1,12 @@
 setwd("~/CP/WES/burden/Misense/REVEL/")
-gl<-read.table("~/CP/WES/burden/genelist")$V1
-LoF<-read.table("CP1578_m6_CT2028.REVEL75.txt",head=F,sep="\t")
-LoF<-LoF[LoF$V9%in%gl,]
-res<-character()
-genelist=unique(LoF$V9)
-for (i in genelist) {
-  gene=LoF[LoF$V9==i,]
-  if("CP"%in%gene$V1){
-    cp=sum(gene[gene$V1=="CP","V197"])*2
-  }else{
-    cp=0
-  }
-  if("CTRL"%in%gene$V1){
-    ctrl=sum(gene[gene$V1=="CTRL","V197"])*2
-  }else{
-    ctrl=0
-  }
-  if(ctrl==0){
-    res[gene$V9]=gene$V9
-  }
-}
-
-LoF<-LoF[LoF$V1=="CTRL",]
-a<-table(LoF$V9,LoF$V2)
-a<-rowSums(a)==0
-
-
-
-LoF_counts<-table(LoF$V12)
-LoF_counts[LoF_counts>=3]
-splicing_counts<-table(LoF$V10)
-LoF1<-LoF[!LoF$V12%in%names(LoF_counts[LoF_counts>=3])[2:865],]
-LoF2<-LoF1[!LoF1$V10%in%names(splicing_counts[splicing_counts>=3])[2:247],]
+LoF<-read.table("CPCTRL.REVEL75.txt",head=F,sep="\t")
 genelist<-unique(LoF$V9)
 p<-numeric()
 or<-numeric()
 h<-numeric()
 l<-numeric()
+num_cases = 
+num_ctrl =
 for (i in genelist) {
   gene=LoF[LoF$V9==i,]
   if("CP"%in%gene$V1){
@@ -49,16 +19,13 @@ for (i in genelist) {
   }else{
     ctrl=0
   }
-  fisher_res=fisher.test(matrix(c(cp,1572*2-cp,ctrl,2028*2-ctrl),2), alternative = 'greater')
+  fisher_res=fisher.test(matrix(c(cp,num_cases*2-cp,ctrl,num_ctrl*2-ctrl),2), alternative = 'greater')
   p[i]=fisher_res$p.value
   or[i]=fisher_res$estimate
   l[i]=fisher_res$conf.int[1]
   h[i]=fisher_res$conf.int[2]
 }
 res=cbind(p,or,l,h)
-write.table(res,"CP1578NotInCtrl_revel75.txt",quote = F)
-
-library(readxl)
 
 
 
